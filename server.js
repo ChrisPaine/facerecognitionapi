3fe4dc12911b1eff4450ee98cfa45ad19157a147
Chris Paine <cdpaine2003@gmail.com>
2020-08-06 19:27:23 -0400

    dynamic port set in server.js

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
 app.put('/image', (req, res) => { image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000...\n');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Listening on port ${process.env.PORT}...`);
 });
 
 /*Endpoints
