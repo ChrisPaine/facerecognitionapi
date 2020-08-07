c40b19b6cc821fbef1a6bd0fb949ed4e42ed4fd8
Chris Paine <cdpaine2003@gmail.com>
2020-08-07 09:30:15 -0400

    trying the 317 fix

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 // 127.0.0.1 = localhost
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
     client: 'pg',
     connection: {
