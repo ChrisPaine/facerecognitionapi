e118c03f252cf7777ec12f3b94b55523e62ce149
Chris Paine <cdpaine2003@gmail.com>
2020-08-07 09:05:03 -0400

    update url in server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-animated-43910',
       user : 'postgres',
       password : 'admin',
       database : 'test'
